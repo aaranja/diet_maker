43c4a8ba72ea45e83f2f4354bb14769792409730
aaranja <fidxle@gmail.com>
2020-12-08 19:25:46 -0800

    - Ventana emergente agregada

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,6 +12,8 @@ import sample.lista.AlimentoItem;
 import sample.lista.AlimentoListViewCell;
 
 import java.net.URL;
+import java.text.MessageFormat;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -26,13 +28,34 @@ public class Controller implements Initializable {
 
     /* Método para añadir un nuevo alimento a la lista de alimentos seleccionados*/
     public void addAlimento(ActionEvent e) {
-        alimentoObservableList.add(new AlimentoItem(new Alimento("Papas", 200, "Tuberculo"), "500gr"));
+        Alimento alimento = new Alimento("Papas", 200, "Tuberculo");
+
+        alimentoObservableList.add(new AlimentoItem(alimento, "500gr"));
+        TextInputDialog dialog = new TextInputDialog("0");
+        dialog.setHeaderText("Introduce los gramos de "+alimento.getNombre());
+        dialog.setTitle("Alimento: "+alimento.getNombre());
+        dialog.setContentText("Gramos: ");
+
+        Optional<String> result = dialog.showAndWait();
+
+        String s = result.map(r -> {
+            try {
+                Integer n = Integer.valueOf(r);
+                return MessageFormat.format("Nice! I like {0} too!", n);
+            } catch (NumberFormatException ex) {
+                return MessageFormat.format("Unfortunately \"{0}\" is not a int!", r);
+            }
+        }).orElse("You really don't want to tell me, huh?");
+
+        System.out.println(s);
+
     }
 
     /* Método para inicializar el controlador una vez cargado los componentes */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         /* Añadir alimentos a la lista*/
+
         lista_alimentos.setItems(alimentoObservableList);
         /* Darle propiedades a cada fila de la lista*/
         lista_alimentos.setCellFactory(alimentoListView -> {
--- src/sample/buscador/Alimento.java
+++ src/sample/buscador/Alimento.java
@@ -1,13 +1,11 @@
 package sample.buscador;
 
 public class Alimento {
-    private Integer index = 0;
     private String nombre;
     private String tipo_alimento;
     private Integer calorias;
 
     public Alimento(String nombre, int calorias, String tipo_alimento ) {
-        this.index++;
         this.nombre = nombre;
         this.calorias = calorias;
         this.tipo_alimento = tipo_alimento;
@@ -20,8 +18,6 @@ public class Alimento {
     public int getCalorias(){
         return calorias;
     }
-
-    public int getIndex(){ return index; }
 
     public void setCalorias(int nuevaCalorias){
         this.calorias = nuevaCalorias;
